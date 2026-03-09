74088abf483c9c55ab87f8123cb5f4a33522e9b1
RISHI NAVROOP <rishinavroop@gmail.com>
2026-03-10 03:01:19 +0530

    Model Training

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,54 @@
 import os
 import sys
 import dill
-from exception import CustomException
 import pandas as pd
 import numpy as np
 
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 
-def save_object(file_path,obj):
-    try:
-        dir_path=os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from src.exception import CustomException
+import sys
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+
+            para = param[model_name]
+
+            gs = GridSearchCV(model, para, cv=3)
+
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
